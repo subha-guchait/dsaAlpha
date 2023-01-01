119ca0770a89eeb2e2a69ac78a5ff1fac703d566
Subhankar <guchaitsubhankar@gmail.com>
2023-01-01 19:20:58 +0530

    calculate sum function

---
+++ functionAndMethods/calculateSum.java
@@ -0,0 +1,11 @@
+public class calculateSum {
+
+    public static int sumCalculate(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sumCalculate(25, 56));
+
+    }
+}
