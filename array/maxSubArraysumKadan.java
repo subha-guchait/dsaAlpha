43208a3958b573d35f1270312f660e61cd54e1b4
Subhankar <guchaitsubhankar@gmail.com>
2023-01-08 22:39:13 +0530

    trapped rain water

--- array/maxSubArraysumKadan.java
+++ array/maxSubArraysumKadan.java
@@ -1,12 +1,13 @@
 public class maxSubArraysumKadan {
     public static void maxSum(int numbers[]) {
-        int ms = Integer.MIN_VALUE;
-        int cs = 0;
+        int ms = Integer.MIN_VALUE; // maximum sum
+        int cs = 0; // current sum
 
         for (int i = 0; i < numbers.length; i++) {
             cs = cs + numbers[i];
-            if (cs < 0) {
-                cs = 0;
+            if (cs < 0) { // it will only work for positive array but for negative array. if(cs <
+                          // numbers[i])
+                cs = 0; // cs = nums[i]
             }
             ms = Math.max(cs, ms);
         }
