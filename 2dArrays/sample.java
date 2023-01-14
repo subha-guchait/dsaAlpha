016b50ac807faaca3fce17fb4622c85de162bf18
Subhankar <guchaitsubhankar@gmail.com>
2023-01-14 20:11:03 +0530

    2d array key search

---
+++ 2dArrays/sample.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class sample {
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("fount at cell (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+
+        }
+        System.out.println("key not found");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < matrix.length; i++) { // matrix.length = rows
+            for (int j = 0; j < matrix[0].length; j++) { // matrix[0].length = columns
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();1
+        }
+        search(matrix, 7);
+    }
+}
