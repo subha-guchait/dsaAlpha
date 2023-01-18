c0a8b72f0d2304014c6291c98c93d91240cd1ce9
Subhankar <guchaitsubhankar@gmail.com>
2023-01-18 19:55:28 +0530

    string

---
+++ strings/countVowel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class countVowel {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).next();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+
+        }
+        System.out.println("No of vowels: " + count);
+
+    }
+
+}
