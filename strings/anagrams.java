1854b5cef2a34fd090ec5683f1321d6ca47e3668
Subhankar <guchaitsubhankar@gmail.com>
2023-01-19 23:23:10 +0530

    anagram

---
+++ strings/anagrams.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class anagrams {
+    public static boolean chkAnagrams(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if (str1.length() == str2.length()) {
+            char[] str1charArray = str1.toCharArray();
+            char[] str2charArray = str2.toCharArray();
+            Arrays.sort(str1charArray);
+            Arrays.sort(str2charArray);
+            if (Arrays.equals(str1charArray, str2charArray)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(chkAnagrams("amit", "imta"));
+        System.out.println(chkAnagrams("amita", "imta"));
+        System.out.println(chkAnagrams("amit", "mang"));
+    }
+}
